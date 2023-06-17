f21952f7c959752a996a614797a9949385e2f3e2
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2023-06-18 00:40:13 +0530

    part 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'stateful_widget_screen.dart';
 
-import 'home_screen.dart';
+// import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: const StateFulWidget(),
     );
   }
 }
---
+++ lib/stateful_widget_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class StateFulWidget extends StatefulWidget {
+  const StateFulWidget({super.key});
+
+  @override
+  State<StateFulWidget> createState() => _StateFulWidgetState();
+}
+
+class _StateFulWidgetState extends State<StateFulWidget> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    // print('build');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stateful Widget'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(count.toString()),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print(count++);
+          setState(() {
+            
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
