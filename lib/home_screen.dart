f8f7da577c51a4e2ea1a32e3ce962699f9cbe35d
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2023-06-18 00:13:48 +0530

    part 1

---
+++ lib/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  int x = 100;
+
+  @override
+  Widget build(BuildContext context) {
+    print('happy');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Tutorials'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              child: Text(
+                x.toString(),
+                style: const TextStyle(fontSize: 30),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          x++;
+          print(x++);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
